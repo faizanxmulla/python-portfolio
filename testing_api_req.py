c895f38a19257ec48f800d2fae97a110d474c4d2
Faizan Mulla <faizanamulla69@gmail.com>
2024-01-12 00:44:24 +0530

    Start from scratch.

--- testing_api_req.py
+++
@@ -1,9 +0,0 @@
-import requests 
-
-base_url = 'http://127.0.0.1:5000/text-processing'
-
-params = {'text': 'my name is ...   '}
-
-requests = requests.get(base_url, params=params)
-
-print(requests.content, requests.json())

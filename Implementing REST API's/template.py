90f2566e8bec206d9746a5aa936efdc82a0f6098
Faizan Mulla <faizanamulla69@gmail.com>
2024-01-13 01:42:59 +0530

    Add: Text Processing API endpoint.

--- Implementing REST API's/template.py
+++ Implementing REST API's/template.py
@@ -9,7 +9,6 @@ swagger = Swagger(app)
 
 class UppercaseText(Resource):
     def get(self):
-
         """
         This method responds to the GET request for this endpoint and returns the data in uppercase.
         ---
@@ -39,7 +38,64 @@ class UppercaseText(Resource):
         return ({"text": text.upper()}), 200
 
 
+class TextProcessing(Resource):
+    def get(self):
+        """
+        This method responds to the GET request for text processing.
+        ---
+        tags:
+        - Text Processing
+        parameters:
+            - name: text
+              in: query
+              type: string
+              required: true
+              description: The text to be processed
+            - name: duplication_factor
+              in: query
+              type: integer
+              required: false
+              description: The number of times to repeat the text
+            - name: capitalization
+              in: query
+              type: string
+              enum: [UPPER, LOWER, NONE]
+              required: false
+              description: The type of capitalization to apply
+        responses:
+            200:
+                description: A successful GET request
+                content:
+                    application/json:
+                      schema:
+                        type: object
+                        properties:
+                            result:
+                                type: string
+                                description: The processed text
+        """
+
+        text = request.args.get("text")
+
+        duplication_factor = int(request.args.get("duplication_factor", 1))
+
+        capitalization = request.args.get("capitalization", "NONE").upper()
+
+        if capitalization == "UPPER":
+            processed_text = text.upper()
+        elif capitalization == "LOWER":
+            processed_text = text.lower()
+        else:
+            processed_text = text
+
+        result = processed_text * duplication_factor
+
+        return {"result": result}, 200
+
+
 api.add_resource(UppercaseText, "/uppercase")
+api.add_resource(TextProcessing, "/text-processing")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Implementing REST API's/test_api_req.py
+++ Implementing REST API's/test_api_req.py
@@ -1,8 +1,12 @@
 import requests
 
-base_url = "http://127.0.0.1:5000/uppercase"
+base_url = "http://127.0.0.1:5000/text-processing"
 
-params = {'text': 'my name is ...'}
+params = {
+    "text": "my name is ...   ",
+    "duplication_factor": 4,
+    "capitalization": "UPPER",
+}
 
 response = requests.get(base_url, params=params)
 

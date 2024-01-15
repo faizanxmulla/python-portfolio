a9d1e06b7e6c6faeb9ac3905f1e61ba5c09cd2fd
Faizan Mulla <faizanamulla69@gmail.com>
2024-01-15 15:10:04 +0530

    change base URL

--- Implementing REST API's/test_api_req.py
+++ Implementing REST API's/test_api_req.py
@@ -1,6 +1,6 @@
 import requests
 
-base_url = "http://127.0.0.1:5000/text-processing"
+base_url = "https://book-review-api-twy8.onrender.com/text-processing"
 
 params = {
     "text": "my name is ...   ",
